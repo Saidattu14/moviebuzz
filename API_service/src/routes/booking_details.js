961e139fc55ee24d462f7f78c9315dcd0758ab79
Saidattu14 <66754678+Saidattu14@users.noreply.github.com>
2021-10-08 15:37:22 +0530

    updated booking_details

--- API_service/src/routes/booking_details.js
+++ API_service/src/routes/booking_details.js
@@ -6,26 +6,7 @@ const { body, query,param} = require('express-validator');
 const apiErrorReporter = require('../utils/api_error_report');
 const kafka = require('../kafka_producer/booking-payment-producer');
 
-router.get(
-    '/:country/:state-and-Where-therters-available-tickets',
-    [
-      param('state'),
-      param('country_name'),
-      query('movie_name'),
-      apiErrorReporter,
-    ],
-    async (req, res, next) => {
-      try {
-        const data = req.body;
-        kafka.movie_data(data,(err) => {
-          console.log(err)
-        })
-        return res.status(201).send('OK');
-      } catch (err) {
-        return next(err);
-      }
-    },
-);
+
 
 /** Example body
  {
